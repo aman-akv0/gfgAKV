f550c5e102fb730c6408367d0cdad420fbf65ead
Aman Kumar <135839236+aman-akv0@users.noreply.github.com>
2024-01-18 22:24:53 +0530

    Update Water the plant.cpp

--- Water the plant.cpp
+++ Water the plant.cpp
@@ -30,3 +30,37 @@ int min_sprinklers(int gallery[], int n)
         
         return ans;
     }
+
+
+
+
+wrong one
+
+
+int min_sprinklers(int gallery[], int n) {
+    int ans = INT_MAX;
+
+    for (int i = 0; i < n; ++i) {
+        int j = 0;
+        int c = 0;
+
+        while (j < n) {
+            if (gallery[j] == -1) {
+                ++j;
+            } else {
+                int a = j - gallery[j];
+                int b = j + gallery[j];
+                if (a < 0) a = 0;
+                if (b > n - 1) b = n - 1;
+                j = b + 1;
+                c++;
+            }
+        }
+
+        // Check if the last element itself covers the entire gallery
+        if (j - 1 >= n - 1) {
+            ans = std::min(ans, c);
+        }
+    }
+
+    return (ans == INT_MAX) ? -1 : ans;
