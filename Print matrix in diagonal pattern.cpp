a42c6ba772e4bc01f6d5d0540e602a919f470964
Aman Kumar <135839236+aman-akv0@users.noreply.github.com>
2024-03-13 22:39:27 +0530

    Create Print matrix in diagonal pattern.cpp

---
+++ Print matrix in diagonal pattern.cpp
@@ -0,0 +1,29 @@
+vector<int> matrixDiagonally(vector<vector<int>>&mat)
+    {
+        bool flag =false;
+        int dig =0;
+        vector<int> res;
+        while(dig <= 2*mat.size()-2){
+            if(flag){
+                for(int i=0; i<=dig; i++){
+                    if(max(dig-i, i) < mat.size()){
+                        res.push_back(mat[i][dig-i]);
+                    }
+                    
+                }
+                flag =false;
+            }
+            else{
+                for(int j=0; j<=dig; j++){
+                    if(max(dig-j, j) < mat.size()){
+                        res.push_back(mat[dig-j][j]);
+                    }
+                }
+                flag =true;
+            }
+            dig++;
+        }
+        return res;
+        
+         //Your code here
+    }
